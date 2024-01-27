cedb9a4b22a54b4bec3a164e79d8fcf681bd39d4
Konstancja Gałat <116946795+konsternacja@users.noreply.github.com>
2024-01-27 08:17:58 +0100

    Delete fluorCAM/analysis.r

--- fluorCAM/analysis.r
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-file_names_70eminus <- paste0("70eminus", 1:5, ".TXT") # create a vector of file names
-list_of_dataframes <- lapply(file_names, function(x) read.csv(x, header = TRUE)) # read each file into a data frame
-combined_df <- do.call(rbind, list_of_dataframes) # combine all data frames
-
-kable(combined_df)
